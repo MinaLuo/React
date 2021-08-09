9ca9f6c5fdbdcf3db3f75bf019875e0590a171ee
MinaLuo <619696434@qq.com>
2021-08-09 22:32:45 +0800

    20210809

--- react-staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
+++ react-staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PubSub from 'pubsub-js';
-// import axios from 'axios';
+import axios from 'axios';
 
 export default class Search extends Component {
 
@@ -9,22 +9,16 @@ export default class Search extends Component {
         const { keyWordElement: { value: keyWord } } = this;
         //发送请求前通知List更新状态
         PubSub.publish('atguigu', { isFirst: false, isLoading: true });
-        //发送网络请求---使axios发送请求
-        // axios.get(`/api1/search/users2?q=${keyWord}`).then(
-        //     response => {
-        //         //请求成功后通知List更新状态
-        //         PubSub.publish('atguigu', { isLoading: false, users: response.data.items });
-        //     },
-        //     error => {
-        //         //请求失败后通知List更新状态
-        //         PubSub.publish('atguigu', { isLoading: false, err: error.message });
-        //     }
-        // )
-
-        //发送网络请求---使fetch发送
-        fetch(`/api1/search/users2?q=${keyWord}`).then(
-            response => { console.log('成功了', response) },
-            error => { console.log('失败了', error) }
+        //发送网络请求
+        axios.get(`/api1/search/users?q=${keyWord}`).then(
+            response => {
+                //请求成功后通知List更新状态
+                PubSub.publish('atguigu', { isLoading: false, users: response.data.items });
+            },
+            error => {
+                //请求失败后通知List更新状态
+                PubSub.publish('atguigu', { isLoading: false, err: error.message });
+            }
         )
     }
 
