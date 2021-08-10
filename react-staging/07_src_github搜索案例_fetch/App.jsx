591fb2db8c05de14538b7286fe8c6f1c6900b5da
MinaLuo <619696434@qq.com>
2021-08-10 21:55:21 +0800

    202108010

---
+++ react-staging/07_src_github搜索案例_fetch/App.jsx
@@ -0,0 +1,16 @@
+//创建”外壳“组件App
+import React, { Component } from 'react';
+import Search from './components/Search';
+import List from './components/List';
+
+//创建并暴露App组件
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Search />
+                <List {...this.state} />
+            </div>
+        )
+    }
+}
