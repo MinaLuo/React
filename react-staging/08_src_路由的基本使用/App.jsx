e3bedd0cdcc4c49a7ea90e5496dc64b523096c67
MinaLuo <619696434@qq.com>
2021-08-14 08:35:47 +0800

    20210814

--- react-staging/08_src_路由的基本使用/App.jsx
+++ react-staging/08_src_路由的基本使用/App.jsx
@@ -1,9 +1,8 @@
 //创建”外壳“组件App
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
-import Home from './pages/Home';//Home是路由组件
-import About from './pages/About';//About是路由组件
-import Header from './component/Header';//Header是一般组件
+import Home from './components/Home';
+import About from './components/About';
 
 //创建并暴露App组件
 export default class App extends Component {
@@ -12,7 +11,7 @@ export default class App extends Component {
             <div>
                 <div className="row">
                     <div className="col-xs-offset-2 col-xs-8">
-                        <Header />
+                        <div className="page-header"><h2>React Router Demo</h2></div>
                     </div>
                 </div>
                 <div className="row">
