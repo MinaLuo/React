ea0d2f3dbfc5277f016cc2379748e7fd0a48f40c
MinaLuo <619696434@qq.com>
2021-08-20 22:28:25 +0800

    20210820

--- redux-test/3_src_redux完整版/components/Count/index.js
+++ redux-test/3_src_redux完整版/components/Count/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //引入store，用于获取redux中保存的状态
 import store from '../../redux/store';
 //引入actionCreator,专门用于创建action对象
-import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count_action';
+import { createIncrementAction, createDecrementAction } from '../../redux/count_action'
 
 export default class Count extends Component {
 
@@ -40,9 +40,9 @@ export default class Count extends Component {
     //异步加
     incrementAsync = () => {
         const { value } = this.selectNumber;
-        // setTimeout(() => {
-        store.dispatch(createIncrementAsyncAction(value * 1, 500))
-        // }, 500)
+        setTimeout(() => {
+            store.dispatch(createIncrementAction(value * 1))
+        }, 500)
     }
 
     render() {
